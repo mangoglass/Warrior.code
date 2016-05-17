8888c242e9f0b0754eb080d870c1b8b8a0e34b80
Andy Pearson <IORoot@users.noreply.github.com>
2016-05-17 14:25:40 +0100

    Created Squat.php

---
+++ squat.php
@@ -0,0 +1,28 @@
+<?php
+
+/* The Warrior Code */
+
+class squat extends posteriorChain {
+
+    use glutes, hamStrings, adductors;
+
+    public function strength(reps) {
+        $sets = 5;
+        $repetitions = 2;
+        $weight = ($oneRepMax / 100) * 90;
+
+        for ($set=0, $set.length, $set+1) {
+            foreach ($repetitions as $rep){
+
+                $check[$rep] = [
+                    "On Bar" => $weight,
+                    "Ass" => "to Grass",
+                    "Chest" => "Proud",
+                    "Body" => "Tight like a tiger."
+                ];
+
+                return "every 4 minutes";
+            }
+        }
+	}
+}
