cdbca7ab24523ab5047626c1ccca4c01c37e79b6
Andy Pearson <IORoot@users.noreply.github.com>
2016-05-17 14:50:14 +0100

    Create muscle-ups.php

---
+++ muscle-ups.php
@@ -0,0 +1,11 @@
+<?php
+/* The Warrior Code */
+
+public function muscleUps( fullRangeOfMotion,
+                           noKipping,
+                           noGloves,
+                           onBar)
+{
+    str_repeat("Eight sets", 2);
+    return "Every two minutes";
+}
