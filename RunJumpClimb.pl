ee16d42f6f6e871c4f47fd3f0ed6317e26c269ed
Andy Pearson <IORoot@users.noreply.github.com>
2016-05-24 09:26:49 +0100

    Updated spacing.

--- RunJumpClimb.pl
+++ RunJumpClimb.pl
@@ -2,8 +2,8 @@
 # The Warrior Code
 $life = <>;
 $life =~ s/#?
-			(?:material|excessive?|narciss(us)?)
-			(ism|ness|ist)?
-		 /running jumping & climbing!\n
-		 /igx;
+	(?:material|excessive?|narciss(us)?)
+	(ism|ness|ist)?
+	/running jumping & climbing!\n
+	/igx;
 print $life;
